c564296df8a46b2bf9363b4b7b432ab01132170b
bbbpppwagg <42564688+bbbpppwagg@users.noreply.github.com>
2018-09-28 15:23:40 +0800

    Update itunes.py

--- crawler/itunes.py
+++ crawler/itunes.py
@@ -1,6 +1,7 @@
 import scrapy
 from tutorial.items import TutorialItem
 from urllib.request import unquote
+from selenium import webdriver
 import re
 
 class ItunesSpider(scrapy.Spider):
@@ -11,7 +12,7 @@ class ItunesSpider(scrapy.Spider):
     ]
 
     def parse(self,response):
-
+        
         app_name = response.xpath(
             '//h1/text()')[0].extract()
         app_name = app_name.replace('\n', '').replace(' ', '')
@@ -35,108 +36,107 @@ class ItunesSpider(scrapy.Spider):
         app_logo_url = app_logo_url.replace('\n', '').replace(' ', '')
         print(app_logo_url)
 
-        app_min_os_version = response.xpath()
+        app_min_os_version = response.xpath(
+            '//div[@class="information-list__item l-row"]//dd/@aria-label')[1].extract()
         app_min_os_version = app_min_os_version.replace('\n', '').replace(' ', '')
+        app_min_os_version = re.findall(r"\d+\.?\d*", app_min_os_version)
+        app_min_os_version = ''.join(app_min_os_version)
         print(app_min_os_version)
 
-        app_min_os_version_text = response.xpath()
+        app_min_os_version_text = response.xpath(
+            '//div[@class="information-list__item l-row"]//dd/@aria-label')[1].extract()
         app_min_os_version_text = app_min_os_version_text.replace(
             '\n', '').replace(' ', '')
         print(app_min_os_version_text)
 
-        app_website = response.xpath()
-        app_website = app_website.replace('\n', '').replace(' ', '')
-        print(app_website)
+        # 不是所有App都有
+        app_purchases_text = response.xpath('//dd[@class="information-list__item__definition l-column medium-9 large-6"]//li//span').extract()
+        # app_purchases_text = app_purchases_text.replace('\n', '').replace(' ', '')
+        for i in app_purchases_text:
+            print(i)
+        print("OK")
 
-        app_support_website = response.xpath()
-        app_support_website = app_support_website.replace(
-            '\n', '').replace(' ', '')
-        print(app_support_website)
-
-        app_purchases_text = response.xpath()
-        app_purchases_text = app_purchases_text.replace('\n', '').replace(' ', '')
-        print(app_purchases_text)
-
-        app_developer_url = response.xpath()
+        app_developer_url = response.xpath('//h2[@class="product-header__identity app-header__identity"]//a/@href')[0].extract()
         app_developer_url = app_developer_url.replace(
             '\n', '').replace(' ', '')
         print(app_developer_url)
 
-        app_content_rating = response.xpath()
+        app_content_rating = response.xpath(
+            '//h1[@class="product-header__title app-header__title"]//span/text()')[0].extract()
         app_content_rating = app_content_rating.replace(
             '\n', '').replace(' ', '')
         print(app_content_rating)
 
-        app_content_rating_reason = response.xpath()
-        app_content_rating_reason = app_content_rating_reason.replace(
-            '\n', '').replace(' ', '')
-        print(app_content_rating_reason)
+        # app_content_rating_reason = response.xpath()
+        # app_content_rating_reason = app_content_rating_reason.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_content_rating_reason)
 
-        app_installation_size = response.xpath()
-        app_installation_size = app_installation_size.replace(
-            '\n', '').replace(' ', '')
-        print(app_installation_size)
+        # app_installation_size = response.xpath()
+        # app_installation_size = app_installation_size.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_installation_size)
 
-        app_language = response.xpath()
-        app_language = app_language.replace('\n', '').replace(' ', '')
-        print(app_language)
+        # app_language = response.xpath()
+        # app_language = app_language.replace('\n', '').replace(' ', '')
+        # print(app_language)
 
-        app_update_text = response.xpath()
-        app_update_text = app_update_text.replace('\n', '').replace(' ', '')
-        print(app_update_text)
+        # app_update_text = response.xpath()
+        # app_update_text = app_update_text.replace('\n', '').replace(' ', '')
+        # print(app_update_text)
 
-        app_comments = response.xpath()
-        app_comments = app_comments.replace('\n', '').replace(' ', '')
-        print(app_comments)
+        # app_comments = response.xpath()
+        # app_comments = app_comments.replace('\n', '').replace(' ', '')
+        # print(app_comments)
 
-        app_deal_comments = response.xpath()
-        app_deal_comments = app_deal_comments.replace(
-            '\n', '').replace(' ', '')
-        print(app_deal_comments)
+        # app_deal_comments = response.xpath()
+        # app_deal_comments = app_deal_comments.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_deal_comments)
 
-        app_and_other_apps = response.xpath()
-        app_and_other_apps = app_and_other_apps.replace(
-            '\n', '').replace(' ', '')
-        print(app_and_other_apps)
+        # app_and_other_apps = response.xpath()
+        # app_and_other_apps = app_and_other_apps.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_and_other_apps)
 
-        app_download_number = response.xpath()
-        app_download_number = app_download_number.replace(
-            '\n', '').replace(' ', '')
-        print(app_download_number)
+        # app_download_number = response.xpath()
+        # app_download_number = app_download_number.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_download_number)
 
-        app_all_version_rating = response.xpath()
-        app_all_version_rating = app_all_version_rating.replace(
-            '\n', '').replace(' ', '')
-        print(app_all_version_rating)
+        # app_all_version_rating = response.xpath()
+        # app_all_version_rating = app_all_version_rating.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_all_version_rating)
 
-        app_all_version_rating_number = response.xpath()
-        app_all_version_rating_number = app_all_version_rating_number.replace(
-            '\n', '').replace(' ', '')
-        print(app_all_version_rating_number)
+        # app_all_version_rating_number = response.xpath()
+        # app_all_version_rating_number = app_all_version_rating_number.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_all_version_rating_number)
 
-        app_current_version_rating = response.xpath()
-        app_current_version_rating = app_current_version_rating.replace(
-            '\n', '').replace(' ', '')
-        print(app_current_version_rating)
+        # app_current_version_rating = response.xpath()
+        # app_current_version_rating = app_current_version_rating.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_current_version_rating)
 
-        app_current_version_rating_number = response.xpath()
-        app_current_version_rating_number = app_current_version_rating_number.replace(
-            '\n', '').replace(' ', '')
-        print(app_current_version_rating_number)
+        # app_current_version_rating_number = response.xpath()
+        # app_current_version_rating_number = app_current_version_rating_number.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_current_version_rating_number)
 
-        app_version_name = response.xpath()
-        app_version_name = app_version_name.replace('\n', '').replace(' ', '')
-        print(app_version_name)
+        # app_version_name = response.xpath()
+        # app_version_name = app_version_name.replace('\n', '').replace(' ', '')
+        # print(app_version_name)
 
-        app_update_time = response.xpath()
-        app_update_time = app_update_time.replace('\n', '').replace(' ', '')
-        print(app_update_time)
+        # app_update_time = response.xpath()
+        # app_update_time = app_update_time.replace('\n', '').replace(' ', '')
+        # print(app_update_time)
 
-        app_copyright_text = response.xpath()
-        app_copyright_text = app_copyright_text.replace(
-            '\n', '').replace(' ', '')
-        print(app_copyright_text)
+        # app_copyright_text = response.xpath()
+        # app_copyright_text = app_copyright_text.replace(
+        #     '\n', '').replace(' ', '')
+        # print(app_copyright_text)
 
-        app_url_id = response.xpath()
-        app_url_id = app_url_id.replace('\n', '').replace(' ', '')
-        print(app_url_id)
+        # app_url_id = response.xpath()
+        # app_url_id = app_url_id.replace('\n', '').replace(' ', '')
+        # print(app_url_id)
